9624363678a7c3a05f1e749bb7c0f591cc5fe3cc
Steve Motsco <stephenmotsco@g.harvard.edu>
2014-12-18 09:59:56 -0600

    Corrected env.php file.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,25 +24,26 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-# $env = $app->detectEnvironment(function() {
-# 
-#     # See if there's an environment.php file...
-#     # If it exists, get environment string from there
-#     if(file_exists(__DIR__.'/../environment.php')) {
-#         return require __DIR__.'/../environment.php';
-#     }
-#     # If not found, assume production
-#     else {
-#         return 'production';
-#     }
-# });
+ $env = $app->detectEnvironment(function() {
+ 
+     # See if there's an environment.php file...
+     # If it exists, get environment string from there
+     if(file_exists(__DIR__.'/../environment.php')) {
+         return require __DIR__.'/../environment.php';
+     }
+     # If not found, assume production
+     else {
+         return 'production';
+     }
+ });
 
-$env = $app->detectEnvironment(array(
+/*$env = $app->detectEnvironment(array(
 
 	'local' => array('*')
 	//'production' => array('*')
 
-));
+));*/
+
 /*
 |--------------------------------------------------------------------------
 | Bind Paths
